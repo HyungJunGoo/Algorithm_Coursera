1805aaf54fa68ec4601f782e4d3a23fd60d63f51
HyungJun Goo <9h0jun1115@gmail.com>
2020-02-26 23:20:46 +0900

    2.26

--- quiz2_count_inversion.cpp
+++ quiz2_count_inversion.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 #include <fstream>
+#define SIZE 100000
 using namespace std;
 
+long int split_count(int arr[], int low, int mid, int len);
 
-long long split_count(int arr[], int low, int mid, int len);
-
-// void swap(int* a, int* b)
-// {
-//     int temp = *a;
-//     *a = *b;
-//     *b = temp;
-// }
-
-long long count(int arr[], int start, int end)
+long int count(int arr[], int start, int end)
 {
-    if (start == end) return 0;
-    int mid = start + (end - start)/2;
-    long long lft_splt = count(arr, start, mid);
-    long long rgt_splt = count(arr, mid+1, end);
-    long long splt_cnt = split_count(arr, start, mid, end);
-    // cout << "-----" << endl;
-    return lft_splt + rgt_splt + splt_cnt;
+    if (start < end)
+    {
+        int mid = start + (end - start)/2;
+        long int lft_splt = count(arr, start, mid);
+        long int rgt_splt = count(arr, mid+1, end);
+        long int splt_cnt = split_count(arr, start, mid, end);
+        return lft_splt + rgt_splt + splt_cnt;
+    }
+    return 0;
 }
 
-long long split_count(int arr[], int start, int mid, int end)
+long int split_count(int arr[], int start, int mid, int end)
 {
     int n1 = mid - start + 1;
     int n2 = end - mid;
@@ -38,7 +33,7 @@ long long split_count(int arr[], int start, int mid, int end)
 
     int i = 0, j = 0;
     int k = start;
-    long long count = 0;
+    long int count = 0;
 
     while (i < n1 && j < n2)
     {
@@ -47,7 +42,6 @@ long long split_count(int arr[], int start, int mid, int end)
             arr[k] = leftArr[i];
             i++;
         }
-
         else
         {
             arr[k] = rightArr[j];
@@ -58,27 +52,57 @@ long long split_count(int arr[], int start, int mid, int end)
     }
     while (i < n1)
     {
-        arr[k] = rightArr[j];
-        k++; j++;
+        arr[k] = rightArr[i];
+        k++;
+        i++;
     }
 
     while (j < n2)
     {
         arr[k] = rightArr[j];
-        k++; j++;
-        count += n1 - i;
+        k++;
+        j++;
+        //count += n1 - i;
     }
     delete [] leftArr;
     delete [] rightArr;
 
     return count;
-    
 }
 
-int main()
+void convStrToInt(string arr[], int arr2[])
 {
-    int testarr[4] = {1, 2, 3, 4};
-    long long answer = count(testarr, 0, 4);
+  for (int j = 0; j < SIZE; j++)
+  {
+    arr2[j] = stoi(arr[j]);
+  }
+}
+
+int main(void)
+{
+    ifstream ifs;
+    int length = SIZE;
+    long long answer;
+    ifs.open("IntegerArray.txt");
+
+    string arr[SIZE];
+    int num = 0;
+    while (!ifs.eof())
+    {
+        getline(ifs, arr[num]);
+        num++;
+    }
+    int arr2[SIZE];
+    convStrToInt(arr, arr2);
+
+    answer = count(arr2, 0, length);
     cout << answer << endl;
+
+
+
+
+    int testarr[4] = {1, 2, 3, 4};
+    long long answer2 = count(testarr, 0, 4);
+    cout << answer2 << endl;
     return 0;
 }
