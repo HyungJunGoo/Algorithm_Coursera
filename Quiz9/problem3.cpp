1ee899d53dadbbdae48bfcc215a3ff7305c4883d
HyungJun Goo <9h0jun1115@gmail.com>
2020-04-15 23:41:07 +0900

    Apr 15

--- Quiz9/problem3.cpp
+++ Quiz9/problem3.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 using pi = pair<int, int>;
-#define INF INT_MAX
 class Graph
 {
     int V; // # of vertices
-    vector<pi>* adj;
+    vector<pi>* adj; // vertex - vertex (cost)
+    int total_cost ;
     vector<bool> Selected;
-    vector<pi> edges;
-    int total_cost;
+    vector<pi> temp;
 public:
     Graph(int V);
-    void init_MST(int MST[]);
     void addEdge(int v, int w, int cost);
-    int primMST(int src);
+    void primMST(int src);
+    void showConndected(int vertex);
+    int getTotalCost();
 };
 
 Graph::Graph(int V)
 {
     this->V = V;
-    adj = new vector<pi>[V+1];
-    Selected = vector<bool> (V+1, false);
-    total_cost = 0; 
-    vector<pi> edges;
+    adj = new vector<pi>[V];
+    vector<bool> Selected (V, false);
+    total_cost = 0;
+    
 }
 
 void Graph::addEdge(int v, int w, int cost)
@@ -35,31 +35,51 @@ bool sortbysec(const pi &a, const pi &b)
     return a.second < b.second;
 }
 
-int Graph::primMST(int src)
+void Graph::primMST(int src)
 {
+    
     Selected[src] = true;
+    cout << "SSI BAL" <<endl;
+    for (vector<pi>::iterator itr = adj[src].begin(); itr != adj[src].end(); itr++)
+    {
+        if(Selected[(*itr).first] != true)
+            temp.push_back(make_pair((*itr).first , (*itr).second));
+    }
+    sort(temp.begin(), temp.end(), sortbysec);
     
+    if(!temp.empty())
+    {
+        int next = temp.front().first;
+        total_cost += temp.front().second;
+        vector<pi>::iterator itr = temp.begin();
+        
+        while (itr != temp.end())
+        {
+            if( (*itr).first == next)
+            {
+                temp.erase(itr);
+                continue;
+            }
+            itr ++;
+        }
+        primMST(next);
+    }
+}
+
+void Graph::showConndected(int vertex)
+{   
+    cout << "From the " << vertex << " there are : ";
     vector<pi>::iterator itr;
-    for (itr = adj[src].begin(); itr != adj[src].end(); itr++)
+    for (itr = adj[vertex].begin(); itr != adj[vertex].end(); itr++)
     {
-        if(Selected[(*itr).first] == false)
-            edges.push_back((*itr));
-    }
-    while (!edges.empty())
-    {
-        sort(edges.begin(), edges.end(), sortbysec);
-        auto x = edges.begin(); edges.erase(edges.begin());
-        total_cost += (*x).second;
-        primMST((*x).first);
-    }
-    
-
-    // sort(edges.begin(), edges.end(), sortbysec);
+        cout << " " ;
+        cout << (*itr).first << " cost : " << (*itr).second << endl;
+    }   
+}
 
 
-    // int next = edges.front().first;
-    // total_cost += edges.front().second;
-    // primMST(next);
+int Graph::getTotalCost()
+{
     return total_cost;
 }
 
@@ -68,9 +88,9 @@ int main()
     fstream file;
     file.open("edges.txt");
     // file.open("sample2.txt");
-    int v, e;
-    file >> v >> e; // v = 500,  e = 2184
-    Graph g(v);
+    int V, e;
+    file >> V >> e; // v = 500,  e = 2184
+    Graph g(V);
 
     while (file.peek() != EOF)
     {
@@ -78,9 +98,11 @@ int main()
         file >> v >> w >> cost;
         g.addEdge(v, w, cost);
     }
-    
-    int answer = g.primMST(1);
-    cout << answer << endl;
+    g.showConndected(4);
+
+    g.primMST(1);
+    //int result = g.getTotalCost();
+    //cout << result << endl;
 
     return 0;
 }
--- Quiz9/test.cpp
+++ Quiz9/test.cpp
@@ -1,20 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main()
 {
-    auto cmp = [](const pair<int, int>& lhs, const pair<int, int>& rhs)
-    {
-    return lhs.second < rhs.second;
-    };
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-
-    pq.push(make_pair(1, 2));
-    pq.push(make_pair(2, 6));
-    pq.push(make_pair(3, 4));
-    pq.push(make_pair(6, 1));
-
-    cout << pq.top().first << endl;
+    vector<bool> a (4, false);
+    a[1] = true;
+    if(a[0] == true)
+        cout << "check " << endl;
+    
     return 0;
 }
